d504b233db0b410bee75ca1f569b26c674922dfa
Alireza Akrami <akrami.ui@gmail.com>
2020-04-19 03:57:44 +0430

    some ui polish

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,19 @@ const Navbar = () => {
     let location = useLocation();
 
     return (
-        <Menu secondary className="main-navbar">
+        <Menu className="main-navbar">
             <Container>
                 <Menu.Item header>
                     <Header as="h3" content="Expense Tracker" />
                 </Menu.Item>
-                <Menu.Item active={location.pathname === "/"} link>
+                {/* <Menu.Item active={location.pathname === "/"} link>
                     <Link to="/"><Icon name="warehouse" /> Home</Link>
                 </Menu.Item>
                 <Menu.Item active={location.pathname.startsWith("/calendar")} link>
                     <Link to="/calendar"><Icon name="calendar outline" /> Calendar</Link>
-                </Menu.Item>
+                </Menu.Item> */}
+                <Link to="/" className={location.pathname==='/'?'item active':'item'}><Icon name="warehouse" /> Home</Link>
+                <Link to="/calendar" className={location.pathname.startsWith("/calendar")?'item active':'item'}><Icon name="calendar alternate outline" /> Calendar</Link>
             </Container>
         </Menu>
     );
