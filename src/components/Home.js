95116656f5da4fcbd8527f4f48a69ce4352aab36
Alireza Akrami <akrami.ui@gmail.com>
2020-04-19 04:14:54 +0430

    lables

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import { Grid, Header, Segment } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Grid, Header, Segment, Statistic } from 'semantic-ui-react';
 
 const Home = (props) => {
-
+    const [total] = useState(2200);
 
     return (
         <>
-        <Grid columns={2}>
-            <Grid.Row>
-                <Grid.Column>
-                    <Header as="h4" content="Total" className="fade" />
-                    <Segment>
-                    </Segment>
-                </Grid.Column>
-                <Grid.Column>
-                    <Header as="h4" content="Income vs Expense" className="fade" />
-                    <Segment>
-                    </Segment>
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
-        <Header as="h4" content="History" className="fade" />
-        <Segment>
-        </Segment>
+            <Grid columns={2}>
+                <Grid.Row>
+                    <Grid.Column>
+                        <Header as="h4" content="CASH" className="fade" />
+                        <Segment>
+                            <Statistic color={total>0?'blue':'red'}>
+                                <Statistic.Value>{total}$</Statistic.Value>
+                            </Statistic>
+                        </Segment>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Header as="h4" content="FLOW" className="fade" />
+                        <Segment>
+                        </Segment>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+            <Header as="h4" content="History" className="fade" />
+            <Segment>
+            </Segment>
         </>)
 
 }
