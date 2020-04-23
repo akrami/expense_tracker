e50adba33fce2245cc4c23bd9e254355f2a36cad
Alireza Akrami <akrami.ui@gmail.com>
2020-04-23 22:41:49 +0430

    ui optimization

--- src/components/home/Total.js
+++ src/components/home/Total.js
@@ -13,7 +13,7 @@ const Total = props => {
 
             var x = d3.scaleLinear()
                 .domain([0, count - 1])
-                .range([0, svg.node().getBoundingClientRect().width]);
+                .range([0, svg.node().getBoundingClientRect().width+2]);
 
             var y = d3.scaleLinear()
                 .domain(d3.extent(days, (d, i) => {
@@ -29,33 +29,16 @@ const Total = props => {
             svg.append("path")
                 .attr("class", "line plus")
                 .attr("d", line(days));
-            svg.append("linearGradient")
-                .attr("id", "line-gradient")
-                .attr("gradientUnits", "userSpaceOnUse")
-                .attr("x1", 0).attr("y1", y(-500))
-                .attr("x2", 0).attr("y2", y(500))
-                .selectAll("stop")
-                .data([
-                    { offset: "0%", color: "#ff668c" },
-                    { offset: "100%", color: "#8ac9e2" }
-                ])
-                .enter()
-                .append("stop")
-                .attr("offset", d => d.offset)
-                .attr("stop-color", d => d.color);
+            
         }
     }, [days]);
 
     return (
-        <>
-            <Header as="h4" content="CASH" className="fade" />
             <Segment id="d3-30-days" >
+                <Header as="h3" content="Total Cash"/>
                 <svg></svg>
-                <Statistic color={total > 0 ? 'blue' : 'red'}>
-                    <Statistic.Value>{total}$</Statistic.Value>
-                </Statistic>
+                <p>{total} $</p>
             </Segment>
-        </>
     );
 }
 
